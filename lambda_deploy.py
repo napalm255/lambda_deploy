ed1acb723ca3c334d1cb0c6b67f97949803440de
napalm255 <napalm255@gmail.com>
2016-03-22 15:16:50 -0400

    initial commit of lambda_deploy.py

---
+++ lambda_deploy.py
@@ -0,0 +1,29 @@
+"""
+lambda function deploy
+"""
+
+from __future__ import print_function
+import boto3
+
+WATCH = [{'bucket'  : 'ask-ava',
+          'key'     : 'ask-ava.zip',
+          'function': 'ask-ava'
+         }]
+
+def lambda_handler(event, context):
+    """ lambda handler """
+    status = None
+    key = event['Records'][0]['s3']['object']['key']
+    bucket = event['Records'][0]['s3']['bucket']['name']
+
+    client = boto3.client('lambda')
+
+    for item in WATCH:
+        if bucket in item['bucket'] and key in item['key']:
+            response = client.update_function_code(
+                FunctionName=item['function'],
+                S3Bucket=item['bucket'],
+                S3Key=item['key']
+            )
+            status = True
+    return status
